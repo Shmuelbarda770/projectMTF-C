7dfb122049379a0910cdd53f980f15a45d2ddc1e
israel <hartl123452019@gmail.com>
2024-09-25 11:34:23 +0300

    chened to alen theme

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + ggggggggg + React</h1>
+      <h1>Vite 👽 + gggg👽ggggg + 👽 React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
